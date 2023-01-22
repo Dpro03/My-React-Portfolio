47639f61b5933362837603f95b813a3fad90f76b
David Proto <davidproto@sbcglobal.net>
2023-01-22 02:33:37 -0500

    styling changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,25 +5,34 @@ const About = () => {
     <div name="About" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className='sm:text-right pbb-8 pl-4'>
-            <p className='sm:text-right text-4xl font-bold inline border-b-4  border-pink-600'>About</p>
+          <div className="sm:text-right pbb-8 pl-4">
+            <p className="sm:text-right text-5xl font-bold inline border-b-4  border-pink-600">
+              About
+            </p>
           </div>
           <div></div>
         </div>
-          
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 pt-4'>
-           <div className='text-4xl font-bold'>
-            <p>Hi, I'm Dave...nice to meet you. Please take a look around.</p>
-           </div>
-            <div>
-              <p>I am passionate about building software that improves the lives of those around me.  I specialize in developing
-                 websites for small businesses and individuals.  I am a full stack web developer with a focus on the front end.  
-                 I am a graduate of the University of Connecticut's Full Stack Web Development Bootcamp.  I am currently looking for a full time position as a web developer.  
-                 Please feel free to contact me if you have any questions or if you would like to work together.</p>
-            
+
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 pt-4">
+          <div className="text-4xl font-bold">
+            <p>
+              Hi, I'm Dave... It's nice to meet you! Please take a look
+              around...
+            </p>
           </div>
-
-
+          <div>
+            <p>
+              This is new to me. My life for the past 20 plus years has been
+              working as a respiratory therapist. Being a healthcare
+              professional had been an amazing experience for me. In my career,
+              I was able to precept new members, including doctors, to life as
+              teammates in improving the health of others. I am a graduate of
+              the University of Connecticut's Full Stack Web Development
+              Bootcamp. I am currently looking for a full time position as a web
+              developer. Please feel free to contact me if you have any
+              questions or if you would like to work together.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/work.jsx
+++ src/components/work.jsx
@@ -8,12 +8,12 @@ import passwordGenerator from "../assets/passwordGenerator.png";
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+          <p className="text-5xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className="py-6">Check out some of my recent work</p>
+          <p className="py-6 text-2xl">Check out some of my recent work</p>
         </div>
         {/* container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
