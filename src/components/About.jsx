9a9c26e840ba5cbf615098b8d9f65cd08716ca73
David Proto <davidproto@sbcglobal.net>
2023-01-28 21:45:20 -0500

    updated about.jsx file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,21 @@ const About = () => {
         </div>
 
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 pt-4">
-          <div className="text-4xl font-bold">
+          <div className="text-4xl font-bold py-4">
             <p>
               Hi, I'm Dave... It's nice to meet you! Please take a look
               around...
             </p>
           </div>
           <div>
-            <p class="text-blue-300">
-              This is new to me. My life for the past 20 plus years has been
-              working as a respiratory therapist. Being a healthcare
-              professional had been an amazing experience for me. In my career,
-              I was able to precept new healthcare team members, including
-              doctors, to help improve the health of other people. I have
-              recently graduated from the University of Connecticut's Full Stack
-              Web Development Bootcamp. I am currently looking for a full time
-              position as a web developer. Please feel free to contact me if you
-              have any questions or if you would like to work together.
+            <p class="text-blue-300 text-left text-2xl py-4 w-full">
+               This is a new venture for me. For the past 20 plus years, I have had the great fortune of 
+               working as a respiratory therapist.  With over two decades of experience
+               in healthcare, I have developed a deep understanding of the industry 
+               and a passion for improving patient outcomes. Having fulfilled my goals in clinical life, I recently completed a 
+               full stack web development bootcamp at the University of Connecticut and am 
+               excited to bring my skills and 
+               expertise to the field of technology. I believe that technology has the power to make a real difference in people's lives and I am passionate about using my new skills to create solutions that have a positive impact on society. I am committed to staying up to date with the latest advancements, and just as I have done in my healthcare career, I will use my skills to help improve the lives of others.  I am a hard worker, a team player, and a problem solver. I am excited to bring my skills and expertise to the field of technology and I look forward to working with you!
             </p>
           </div>
         </div>
